6b784532a8323e679fb838fccb480dca21334c9a
Chris Reagle <chris@ChristophersMBP.localdomain>
2024-09-13 13:33:31 -0400

    Updating Resume

--- generation/create_webserver.js
+++ generation/create_webserver.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-var fs = require('fs');
-var out = fs.openSync('./out.log', 'a');
-var err = fs.openSync('./out.log', 'a');
+const fs = require('fs');
+const out = fs.openSync('./out.log', 'a');
+const err = fs.openSync('./out.log', 'a');
 
-var cp = require('child_process');
-var child = cp.spawn('node', ['generation/webserver.js'], { detached: true, stdio: [ 'ignore', out, err ] });
+const cp = require('child_process');
+const child = cp.spawn('node', ['generation/webserver.js'], { detached: true, stdio: [ 'ignore', out, err ] });
 child.unref();
 
